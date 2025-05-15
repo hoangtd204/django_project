88df60862bdd0afebb0a9ed5b219bb4b40fe9d83
hoangtd <hoangtd@mor.com.vn>
2025-05-15 17:03:36 +0700

    commit day 15/5

--- sms_project/urls.py
+++ sms_project/urls.py
@@ -1,8 +1,12 @@
 from students import views
 from django.contrib import admin
 from django.urls import path
-
+from students.views import student_search_form,create_reverse,student_search_form
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')]
+    path('', views.index, name='index'),
+    path('form/', views.handle_get_post, name='handle_get_post'),
+    path('student/', views.create_reverse, name='student-search-form'),
+    path('student/<str:student_id>/', views.student_search_form, name='show_inf'),
+    ]
 
--- students/views.py
+++ students/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from .models import Student
+
 
 def index(request):
     return render(request, 'students/index.html')
 
+def handle_get_post(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        return HttpResponse(f"You just have send with a name: {name}")
+    else:
+        name = request.GET.get('name', '')
+        return render(request, 'students/forms.html', {'name': name})
+
+
+
+
+
+def student_search_form(request, student_id):
+    context = {}
+    try:
+        student = Student.objects.get(student_id=student_id)
+        context['student'] = student
+    except Student.DoesNotExist:
+        context['error'] = "Student not found"
+
+    return render(request, 'students/show_inf.html', context)
+
+def create_reverse(request):
+    if request.method == 'POST':
+        student_id = request.POST.get('student_id')
+        if student_id:
+            url = reverse('show_inf', kwargs={'student_id': student_id})
+            return redirect(url)
+    return render(request, 'students/student_search.html')
