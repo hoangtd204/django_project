a56c27c1ce0b74d293aaa7cbe0c069080693a146
hoangtd <hoangtd@mor.com.vn>
2025-05-19 17:39:43 +0700

    commit day 19/5

---
+++ students/crud_students/update_student.py
@@ -0,0 +1,4 @@
+
+
+def change_student_inf(student_id_for_updating):
+    pass
--- students/models.py
+++ students/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Student(models.Model):
+    MAJOR_CHOICES = [
+        ("Biotechnology", "Biotechnology"),
+        ("Graphic Design", "Graphic Design"),
+        ("Data Science", "Data Science"),
+        ("Software Engineering", "Software Engineering"),
+        ("Cybersecurity", "Cybersecurity"),
+        ("Cloud Computing", "Cloud Computing"),
+    ]
     student_id = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    major = models.CharField(max_length=100)
+    major = models.CharField(max_length=50, choices=MAJOR_CHOICES)
 
     def __str__(self):
         return f"{self.name} ({self.student_id})"
