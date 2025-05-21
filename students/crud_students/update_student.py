e3029e6d53b89e72d2a4a83b333a3ab1d3cf8ade
hoangtd <hoangtd@mor.com.vn>
2025-05-21 15:01:59 +0700

    commit day 21/5

--- students/crud_students/update_student.py
+++ students/crud_students/update_student.py
@@ -1,39 +1,20 @@
-from students.crud_students.serializers import Student, StudentSerializer
+from students.crud_students.serializers import Student
 from django.forms.models import model_to_dict
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from students.crud_students.serializers import StudentSerializer
+from rest_framework.response import Response
 
-def convert_json_to_dict(request):
-    student = {
-        'student_id': request.POST.get('student_id'),
-        'name': request.POST.get('name'),
-        'age': request.POST.get('age'),
-        'major': request.POST.get('major'),
-    }
-    return student
 
 def find_student_by_id_for_update(student_id):
     try:
         student = Student.objects.get(student_id=student_id)
-        return model_to_dict(student)
+        return True, student
     except Student.DoesNotExist:
-        return "Student not found."
+        return False, None
 
 
-
-
-def change_student_inf(student_target, data_for_update):
-    try:
-        student = Student.objects.get(id=student_target)
-    except ObjectDoesNotExist:
-        return False, "Student not exist "
-
-    for key, value in data_for_update.items():
-        setattr(student, key, value)
-
-    try:
-        student.full_clean()
-    except ValidationError as e:
-        return False, "Update failed"
-    student.save()
-    return True, "Updated Successfully!"
-
+def change_student_inf(request, student):
+    serializer = StudentSerializer(student, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return True, serializer.data
+    return False, serializer.errors
